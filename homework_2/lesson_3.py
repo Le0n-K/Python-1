5d2eb136bdabc2f1458d90e0e004bbc95668d2e9
Le0n-K <leon.kushnir15@gmail.com>
2023-03-03 19:15:54 +0200

    strings

--- homework_2/lesson_3.py
+++ homework_2/lesson_3.py
@@ -2,7 +2,13 @@
 user_input = input(f'Please, input your string:')
 # пользователь пишет "Today is a RainY Day! do you like It? By the Way, i don't."
 user_input = user_input.lower()
-user_input = user_input.strip('.,-:;?!')
+user_input = user_input.strip('.')
+user_input = user_input.strip(',')
+user_input = user_input.strip('-')
+user_input = user_input.strip(':')
+user_input = user_input.strip(';')
+user_input = user_input.strip('!')
+user_input = user_input.strip('?')
 user_input = user_input.rstrip()
 print(user_input)
 
