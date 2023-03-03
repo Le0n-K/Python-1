2acf197e3d9fd28f611e5b8a90764efd64ee9851
Le0n-K <leon.kushnir15@gmail.com>
2023-03-03 21:30:20 +0200

    strings

--- homework_2/lesson_3.py
+++ homework_2/lesson_3.py
@@ -1,23 +1,27 @@
 # пользователь вводит строку
 user_input = input(f'Please, input your string:')
 # пользователь пишет "Today is a RainY Day! do you like It? By the Way, i don't."
-user_input = user_input.lower()
-user_input = user_input.strip('.')
-user_input = user_input.strip(',')
-user_input = user_input.strip('-')
-user_input = user_input.strip(':')
-user_input = user_input.strip(';')
-user_input = user_input.strip('!')
-user_input = user_input.strip('?')
-user_input = user_input.rstrip()
-print(user_input)
+u = user_input.lower()
+u = user_input.strip('.')
+u = user_input.strip(',')
+u = user_input.strip('-')
+u = user_input.strip(':')
+u = user_input.strip(';')
+u = user_input.strip('!')
+u = user_input.strip('?')
+u = user_input.rstrip()
+print(u)
 
-# пользователь выбирает слово "day", поиск слова в предложении
+# пользователь выбирает слово, поиск слова в предложении
 user = input(f'What do you want to replace?')
-a = user_input.find('day')
-print(f'"Day" was found at position {a}')
+a = user_input.find(user)
+print(f'"{user}" was found at position {a}')
 
-# меняем на слово 'night'
+# меняем на слово пользователя
 user_repl = input(f'With what do you want to replace?')
-b = user_input.replace('day', 'night')
+b = user_input.replace(user, user_repl)
 print(input(f'Here is your result:\n {b}'))
+
+
+
+
